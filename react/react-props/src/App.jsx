0e62814c47b440b7e6c5c40b9bbe7d4f775b7fd9
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2023-11-08 15:38:13 +0700

    make a props

--- react/react-props/src/App.jsx
+++ react/react-props/src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import Profile from './components/profile'
 
 function App() {
 
 
   return (
     <>
-
+    <Profile name= "jalil" Username= "Jalilasyakur" Bio="Ini bio" isVerified={true} />
     </>
   )
 }
--- react/react-props/src/components/profile.jsx
+++ react/react-props/src/components/profile.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
-function profile() {
-  return (
-    <div>
-      
-    </div>
-  )
+function Profile({name , Username, Bio, isVerified}) {
+    return (
+        <div className="container">
+            <dl>
+                <dt>Name: </dt>
+                <dd>{name}</dd>
+
+                <dt>Username: </dt>
+                <dd>{Username}</dd>
+
+                <dt>Bio: </dt>
+                <dd>{Bio}</dd>
+
+                <dt>isVerified: </dt>
+                <dd>{isVerified ? 'yes' : 'no'}</dd>
+            </dl>
+        </div>
+    )
 }
 
-export default profile
+export default Profile;
